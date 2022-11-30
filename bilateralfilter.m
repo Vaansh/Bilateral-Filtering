b62d03e1eae36453ed4b643af9d4fa743ae9f479
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2022-11-30 03:11:21 -0500

    minor edits

--- bilateralfilter.m
+++ bilateralfilter.m
@@ -1,22 +1,21 @@
-function O = bilateralfilter(I, sigma_s, sigma_r, window_size)
+function [O] = bilateralfilter(I, sigma_s, sigma_p, window_size)
 % BILATERALFILTER   Performs bilateral filtering on the given image using provided parameters.
 %
 %   Eg. O = BILATERALFILTER(I, 5, 0.1, 0.5) uses a 5x5 filter.
 %
 %   Parameters
 %       I               The observed image matrix 
-%       sigma_s         The sigma_s
-%       sigma_r         The sigma_r
+%       sigma_s         The sandard deviation of the spatial kernel
+%       sigma_p         The sandard deviation of the photometric kernel
 %       window_size     The size of filter window
 % 
 %   Formula
-%       O = 1/W * Σ (G_sigma_s ||p - q||) * (G_sigma_r |I_p - I_q|) * I_q            ... (1)
+%       O = 1/W * Σ (g_s ||p - q||) * (f_r |I_p - I_q|) * I_q                       ... (1)
 %
 %   Notation of Terms
-%       O                           The output image
-%       W                           The normalization factor
-%       G_sigma_s * ||p - q||       The spatial distance (or the space weight)       ... (2)  
-%       G_sigma_r * |I_p - I_q|     The photometric distance (or the range weight)   ... (3)  
+%       O & W               The output image & the normalization factor
+%       g_s ||p - q||       The spatial kernel (or the space weight)                ... (2)  
+%       f_r |I_p - I_q|     The photometric kernel (or the range weight)            ... (3)  
 % 
 %   References
 %       [1] C. Tomasi and R. Manduchi, “Bilateral filtering for gray and color images,” in 
@@ -36,15 +35,11 @@ function O = bilateralfilter(I, sigma_s, sigma_r, window_size)
 
     O = zeros(size(A));
 
-    % Spatial Distance
+    % Spatial Filter
     %   - Computed beforehand.
     %   - Represented by Eq. (2) in Eq. (1)
-    
-    for i = 1 : row
-        for j = 1 : col
-            % Photometric Distance
-            %   - Computed iteratively.
-            %   - Represented by Eq. (3) in Eq. (1)
+    for r = 1 : row
+        for c = 1 : col
         end
     end
 end
